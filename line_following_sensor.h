2498564761e86d455362e8d54ae743bd1b7663a9
JackSparrow-boop <cjulienlaurent@gmail.com>
2025-12-15 11:42:33 +0100

    Update line_following_sensor.h

--- line_following_sensor.h
+++ line_following_sensor.h
@@ -1,1 +1,19 @@
+/*
+ * line_following_sensors.h
+ *
+ *  Created on: Dec 15, 2025
+ *      Author: sparrow.bomkiller11
+ */
+
+#ifndef LINE_FOLLOWING_SENSORS_H_
+#define LINE_FOLLOWING_SENSORS_H_
+
+int return_line_follower_right();
+
+int return_line_follower_left();
+
+
+
+
+#endif /* LINE_FOLLOWING_SENSORS_H_ */
 
